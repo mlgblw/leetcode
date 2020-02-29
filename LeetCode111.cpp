e68b35e6fd02a39cca09f995f9a9ae6001cfed71
mlgblw <1441219867@qq.com>
2020-02-29 19:52:08 +0800

    leetcode

---
+++ LeetCode111.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+
+int minDepth(struct TreeNode* root){
+    if(!root)return 0;
+    if(root->left == NULL && root->right == NULL) return 1;
+    if(root->left == NULL || root->right == NULL) {
+        return minDepth(root->left ? root->left: root->right) + 1;
+    }
+    return fmin(minDepth(root->left),minDepth(root->right)) + 1; 
+}
